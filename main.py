f7d52679b695477bf58f730d3298f591f097f7bf
tsuplige <thomassupligeon@gmail.com>
2023-02-11 22:09:26 +0100

    creation de la fonction FindBookUrl

--- main.py
+++ main.py
@@ -5,10 +5,58 @@ import requests
 
 
 
-url = "http://books.toscrape.com/catalogue/the-girl-in-the-ice-dci-erika-foster-1_65/index.html"
+Burl = "http://books.toscrape.com/catalogue/the-girl-in-the-ice-dci-erika-foster-1_65/index.html"
+Purl = 'http://books.toscrape.com/catalogue/category/books/travel_2/index.html'
 
 
-def FindPageData(page_url):
+
+def FindBookUrl(page_url):
+    links = []
+    page = requests.get(page_url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    artc = soup.find_all('h3')
+
+    for h3 in artc:
+            a = h3.find('a')
+            li = a.get('href')
+            lin = li.replace('../../..', 'http://books.toscrape.com/catalogue')
+            links.append(lin)
+    en_tete = ['liens']
+    with open('link_data.csv', 'w', encoding='utf-8') as csv_files:
+        writer = csv.writer(csv_files, delimiter=',')
+        writer.writerow(en_tete)
+        for link in links:
+            writer.writerow([link])
+
+
+
+
+
+
+
+
+
+
+
+def LinkCatToBook():
+    with open('link_data.csv') as fichier_csv:
+        reader = csv.reader(fichier_csv, delimiter=',')
+        for ligne in reader:
+            print(ligne)
+
+
+
+
+
+
+
+
+
+
+
+
+def FindBookData(page_url):
     page = requests.get(page_url)
     soup = BeautifulSoup(page.content, 'html.parser')
     infotb = soup.find_all('td')
@@ -26,16 +74,20 @@ def FindPageData(page_url):
     category = soup.find('li', class_='active').previous_element.previous_element.previous_element
     review_rating = 0
     img_tag = soup.find('img')
-    image_url = img_tag['src']
+    Iurl = img_tag['src']
+    image_url = Iurl.replace('../..', 'http://books.toscrape.com/')
 
     en_tete = ['url', 'upc', 'title', 'price_including_tax', ' price_excluding_tax', 'number_available', 'product_description', 'category', 'review_rating', 'img_url']
 
-    with open('data.csv', 'w', encoding='utf-8') as csv_files:
+    with open('Book_data.csv', 'w', encoding='utf-8') as csv_files:
         writer = csv.writer(csv_files, delimiter=',')
         writer.writerow(en_tete)
-        writer.writerow([url, upc, title, price_including_tax, price_excluding_tax, number_available, product_description, category, review_rating, image_url])
+        writer.writerow([page_url, upc, title, price_including_tax, price_excluding_tax, number_available, product_description, category, review_rating, image_url])
     
 
 
+FindBookUrl(Purl)
+# FindBookData(Burl)
+LinkCatToBook()
 
-FindPageData(url)
+# FindBookData('http://books.toscrape.com/catalogue/full-moon-over-noahs-ark-an-odyssey-to-mount-ararat-and-beyond_811/index.html')
